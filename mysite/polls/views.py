3a85f83e4fcf5eabe21657b8be71cab1529a7bfb
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-21 14:25:39 -0800

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
