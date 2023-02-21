64562ccf95e0d81370ab51a66e08a8fbbf18cd5c
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-21 14:46:19 -0800

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -18,8 +18,8 @@ def detail(request, question_id):
     return render(request, 'detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -27,7 +27,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
+        return render(request, 'detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
