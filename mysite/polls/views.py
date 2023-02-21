2db991731b2d56b296a486af64d0820cc4cbe413
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-21 13:07:14 -0800

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
