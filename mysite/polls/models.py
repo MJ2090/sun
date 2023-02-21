b26b160d413df284debcd283ad71584b0fba77a9
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-21 12:15:31 -0800

    Update models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,6 +6,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
