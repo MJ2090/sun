cdd1468fc61446d451aff162e811e24a9948efac
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-21 13:14:10 -0800

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,7 +6,7 @@ from django.template import loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    template = loader.get_template('index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
