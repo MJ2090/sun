8a5164b6888ab37e4288acada668ba8c98c90af8
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-21 21:32:00 -0800

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,20 +4,11 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
-import os
-import openai
 
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
-    # Load your API key from an environment variable or secret management service
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    print(api_key)
-
-    response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
-    print(response)
 
     def get_queryset(self):
         """Return the last five published questions."""
