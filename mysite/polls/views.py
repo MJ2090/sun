c4a39341198e353b515ffdf243481cb40cfaf62e
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-21 21:36:12 -0800

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,7 +7,7 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
+    template_name = 'index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
@@ -17,12 +17,12 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = 'detail.html'
 
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/results.html'
+    template_name = 'results.html'
 
 
 def vote(request, question_id):
