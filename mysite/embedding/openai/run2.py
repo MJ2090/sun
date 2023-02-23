5a4fdbebd99d6d0c8965191d04b6d95488c36e21
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-22 16:14:23 -0800

    s

--- mysite/embedding/openai/run2.py
+++ mysite/embedding/openai/run2.py
@@ -7,51 +7,51 @@ import robot
 ################################################################################
 def run_it_2():
     my_text = """
-        ADHD: A four-letter acronym that stands for attention-deficit/hyperactivity disorder. Across our society and throughout modern history, it has also come to represent harmful stereotypes, debilitating assumptions, and unconscious bias.
+        The job cuts in tech land are piling up, as companies that led the 10-year bull market adapt to a new reality.
 
-    Our mission is to empower everyone living with ADHD to reach their fullest potential. We meet that mission by providing a patient-first, technology-powered ADHD treatment platform that keeps costs down and reduces patient wait times.
+Google announced plans to lay off 12,000 people from its workforce Friday, while Microsoft said Wednesday that it’s letting go of 10,000 employees. Amazon
+ also began a fresh round of job cuts that are expected to eliminate more than 18,000 employees and become the largest workforce reduction in the e-retailer’s 28-year history.
 
-    Unchecked and untreated, ADHD can hamper brain development and impede everything from a person’s social skills to their productivity in a professional environment. While the condition itself is considered very common, with more than 3 million cases now diagnosed annually in the U.S. alone, the treatment opportunities for ADHD are anything but common. Most people suffer in silence.
+The layoffs come in a period of slowing growth, higher interest rates to battle inflation, and fears of a possible recession next year.
 
-    We are facing a challenge when it comes to providing psychiatric care, physician burnout, an aging workforce, bureaucratic and insurance demands, and poor compensation. In fact, it’s a simple math equation. In total, more than 20 million people in the U.S. have received a diagnosis of ADHD. But there are fewer than 30,000 psychiatrists to treat them. That means there are roughly 667 patients per psychiatrist. Clearly, unsustainable.
+Here are some of the major cuts in the tech industry so far. All numbers are approximations based on filings, public statements and media reports:
 
-    Beyond addressing the emotional and mental wellness of patients we also see an opportunity to address the financial impact of $150-200 million in lost productivity every year due to ADHD. In addition, no one should lose their job over ADHD, and no one should lose their opportunity to find a meaningful path forward in their professional life as well as their personal one.
+Alphabet: 12,000 jobs cut
+Google, owned by parent company Alphabet, said Friday it will lay off 12,000 people from its workforce.
 
-    Clearly, the time is now to change the course of this treatable condition. The solution?
+Sundar Pichai, Google’s CEO, said in an email sent to the company’s staff that the firm will begin making layoffs in the U.S. immediately. In other countries, the process “will take longer due to local laws and practices,” he said. CNBC reported in November that Google employees had been fearing layoffs as its counterparts made cuts and as employees saw changes to the company’s performance ratings system.
 
-    At Done., first we want to eliminate the stigma and confusion or isolation around ADHD and empower anyone to receive the help they need to live up to their fullest potential. We provide awareness and education for people to learn more, confidential and accurate medical diagnosis through telehealth, and actionable ways to receive immediate treatment.
+Alphabet
+ had largely avoided layoffs until January, when it cut about 240 employees from Verily, its health sciences division.
+ Microsoft: 10,000 jobs cut
+Microsoft is reducing 10,000 workers through March 31 as the software maker braces for slower revenue growth. The company also is taking a $1.2 billion charge.
 
-    We’ve also hired some of the most esteemed and board-certified experts in the industry to collaborate and tackle ADHD together. Their wisdom and experience has allowed us to build a robust online platform that infuses efficiency with effectiveness. We put the patient’s needs first and seek to provide a stable scaffolding during their entire journey with us.
+“I’m confident that Microsoft will emerge from this stronger and more competitive,” CEO Satya Nadella announced in a memo to employees that was posted on the company website Wednesday. Some employees will find out this week if they’re losing their jobs, he wrote.
+Amazon: 18,000 jobs cut
+Earlier this month, Amazon
+ CEO Andy Jassy said the company was planning to lay off more than 18,000 employees, primarily in its human resources and stores divisions. It came after Amazon said in November it was looking to cut staff, including in its devices and recruiting organizations. CNBC reported at the time that the company was looking to lay off about 10,000 employees.
 
-    Done. is dedicated to serving individuals who otherwise may not be comfortable seeking care for ADHD in person due to stigma around ADHD treatment or may not be able to access care due to cost or availability.   By providing a platform that deals directly with patients and manages the logistics and administrative work around their care, Done. allows providers to spend more time focusing on what they do best – evaluating and treating patients.
+Amazon went on a hiring spree during the Covid-19 pandemic. The company’s global workforce swelled to more than 1.6 million by the end of 2021, up from 798,000 in the fourth quarter of 2019.
 
-    Indeed, we believe that technology is a vital part of the solution to connect anyone to the right psychiatric care at the right time - even through their mobile device. This “always-on” strategy allows for seamless accessibility, convenient interactions like video calls, and 24/7 support. Our patients are the lifeblood of Done. and we know that the overwhelming nature of ADHD can strike at any moment.
+Crypto.com: 500 jobs cut
+Crypto.com announced plans to lay off 20% of its workforce Jan. 13. The company had 2,450 employees, according to PitchBook data, suggesting around 490 employees were laid off. 
 
-    There is also a shared responsibility between the patient, the medical practitioner, and Done. to ensure everyone receives responsible and quality treatment in such a timely manner. That treatment can include therapy, medication, or a combination of options depending on the circumstances.
+CEO Kris Marszalek said in a blog post that the crypto exchange grew “ambitiously” but was unable to weather the collapse of Sam Bankman-Fried’s crypto empire FTX without the further cuts.
 
-    Done. does not generate the diagnosis. Rather Done. serves as the ultimate conduit between ailment and treatment; Done.’s clinical operations are managed by an independent professional corporation (PC) headed by board-certified psychiatrists and psychiatric mental health nurse practitioners.
-
-    Our approach is simple:
-
-    1. Visit donefirst.com to take a medically approved assessment test and learn where a patient falls in the spectrum of ADHD (80% of adults with ADHD are not even aware).
-
-    2. Schedule an appointment directly with a medical practitioner and for a patient to discuss their wellness in private.
-
-    3. Once treatment is determined, a patient has the option to join Done. as a valued member of our services and access a broad network of clinicians.
-
-    Of course we do not suggest that our approach is perfect. No medical system in the world functions without difficulties and hurdles along the way. But we know it’s working.
-        """
+“All impacted personnel have already been notified,” Marszalek said in a post.
+"""
 
     my_texts = [("ss", my_text)]
 
     my_df = df.get_df(my_texts)
     qs = ["What day is it?",
-          "What is Done?",
-          "What is ADHD?",
-          "How to treat ADHD?",
-          "How many people have ADHD?",
-          "Where are you",
-          "How do patients like Done.?"]
+          "Which company have laid off people?",
+          "How many people are laid off by Amazon?",
+          "When did Crypto.com lay off people?",
+          "Why Amazon cut staff?",
+          "How is Microsoft doing in the year 2022?",
+          "How do patients like Done.?",
+          "who is Kris Marszalek?"]
     for q in qs:
         print("Question:")
         print(q)
