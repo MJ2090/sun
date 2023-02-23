432740003d35946a9a1b6e531a85580e7f32dbc9
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-23 15:47:04 -0800

    s

--- mysite/embedding/openai/df.py
+++ mysite/embedding/openai/df.py
@@ -103,6 +103,11 @@ def generate_embedding_csv():
     ### Step 7
     ################################################################################
 
+    def myf(x):
+        print('00000000')
+        print(x)
+        return len(tokenizer.encode(x))
+
     # Load the cl100k_base tokenizer which is designed to work with the ada-002 model
     tokenizer = tiktoken.get_encoding("cl100k_base")
 
@@ -112,7 +117,7 @@ def generate_embedding_csv():
     # Tokenize the text and save the number of tokens to a new column
     print('3333333333333333')
     print(df.text)
-    df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+    df['n_tokens'] = df.text.apply(myf)
 
     # Visualize the distribution of the number of tokens per row using a histogram
     df.n_tokens.hist()
