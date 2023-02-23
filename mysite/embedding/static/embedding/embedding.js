85c8c8ba8a3f2b7fc9654715757f1a1521dabae9
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-23 13:20:08 -0800

    s

--- mysite/embedding/static/embedding/embedding.js
+++ mysite/embedding/static/embedding/embedding.js
@@ -1,7 +1,7 @@
 function confirmSubmit(obj) {
   let s = $("#id_message").val();
-  let money = s.length
-  if (confirm(`The training text length is ${s.length}`)) {
+  let money = s.length / 1000 / 100.0
+  if (confirm(`The text length is ${s.length}, which will roughly cost $${money}, continue?`)) {
     return true;
   }
   return false;
