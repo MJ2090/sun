373396ef765a50618a513c7c45b918e5888317f5
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-23 12:03:00 -0800

    s

--- mysite/embedding/static/embedding/embedding.js
+++ mysite/embedding/static/embedding/embedding.js
@@ -1,7 +1,7 @@
 function confirmSubmit(obj) {
-  var s = $("#id_message").val();
-  var money = s.length
-  if (confirm(`The training text length is ${s.length}')) {
+  let s = $("#id_message").val();
+  let money = s.length
+  if (confirm(`The training text length is ${s.length}`)) {
     return true;
   }
   return false;
