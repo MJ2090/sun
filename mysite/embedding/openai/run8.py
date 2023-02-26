6c787ae57b8dcad3b74e1a150962f4b92111c098
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-26 00:55:09 -0800

    s

--- mysite/embedding/openai/run8.py
+++ mysite/embedding/openai/run8.py
@@ -5,7 +5,7 @@ def run_it_8(prompt):
     response = openai.Image.create(
         prompt=prompt,
         n=1,
-        size="1024x1024"
+        size="512x512"
     )
     print(response)
     return response
