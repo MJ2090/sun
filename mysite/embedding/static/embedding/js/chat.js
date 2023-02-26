19612571ccdec1c6697bb0b4adf8d511055ba4b1
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-25 23:19:30 -0800

    s

--- mysite/embedding/static/embedding/js/chat.js
+++ mysite/embedding/static/embedding/js/chat.js
@@ -12,6 +12,7 @@ function async_call() {
             textarea.val( response);
             textarea.prop( "disabled", false );
             textarea.focus();
+            $('.word-count').val(response.length + ' chars');
         },
     })
 }
