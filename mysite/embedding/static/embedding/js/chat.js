f6dac2c66ad4bb774e509ffbc95ca879eaeb98ec
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-25 23:13:25 -0800

    s

--- mysite/embedding/static/embedding/js/chat.js
+++ mysite/embedding/static/embedding/js/chat.js
@@ -1,20 +1,30 @@
+function async_call() {
+    let textarea = $("textarea[name='message']");
+    let csrf = $("input[name='csrfmiddlewaretoken']");
+    textarea.prop( "disabled", true );
+    $.ajax({
+        type: 'POST',
+        url: "/sendchat/",
+        data: {message: textarea.val(),
+                csrfmiddlewaretoken: csrf.val(),
+        },
+        success: function (response) {
+            textarea.val( response);
+            textarea.prop( "disabled", false );
+            textarea.focus();
+        },
+    })
+}
+
 function init() {
     $('.send-button').click(function(){
-        let textarea = $("textarea[name='message']");
-        let csrf = $("input[name='csrfmiddlewaretoken']");
-        textarea.prop( "disabled", true );
-        $.ajax({
-            type: 'POST',
-            url: "/sendchat/",
-            data: {message: textarea.val(),
-                    csrfmiddlewaretoken: csrf.val(),
-            },
-            success: function (response) {
-                textarea.val( response);
-                textarea.prop( "disabled", false );
-                textarea.focus();
-            },
-        })
+        async_call();
+    });
+
+    $("textarea[name='message']").keyup(function(e){
+        if(e.keyCode == 13) {
+            async_call();
+        }
     });
 }
 
