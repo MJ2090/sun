96d6a8e2f5f628ae7eec3f49b086ca16f2b58ff5
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-02-27 13:54:52 -0800

    ss

--- mysite/embedding/forms/contact.py
+++ mysite/embedding/forms/contact.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):        
     username = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Username"}))
     email = forms.EmailField(required=True, max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': "you@example.com"}))
-    message = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Your message"}))
+    message = forms.CharField(required=True, max_length=100, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': "Contents"}))
