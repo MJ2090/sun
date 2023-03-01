d421a6f11e71ca9ce13905f724ad90006c2c8c1f
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-03-01 11:15:51 -0800

    s

--- mysite/fine/fine.py
+++ mysite/fine/fine.py
@@ -1,10 +1,10 @@
 import pandas as pd
-df = pd.read_csv('therapy_3.csv')
+df = pd.read_csv('therapy_2000.csv')
 
 
-for i in range(3):
+for i in range(len(df)):
     df.iloc[i, 0] = df.iloc[i, 0] + "\n\n###\n\n"
     df.iloc[i, 1] = " " + df.iloc[i, 1] + "\n###\n"
-    print(df.iloc[i, 0])
+    #print(df.iloc[i, 0])
     
-df.to_csv('therapy_3_a.csv', index=False)
+df.to_csv('therapy_2000_a.csv', index=False)
