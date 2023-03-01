a626b2775b4f1e2d9fda5d04c0d870b1b176b846
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-03-01 15:23:09 -0800

    s

--- mysite/fine/fine.py
+++ mysite/fine/fine.py
@@ -5,6 +5,5 @@ df = pd.read_csv('therapy_2000.csv')
 for i in range(len(df)):
     df.iloc[i, 0] = df.iloc[i, 0] + "###"
     df.iloc[i, 1] = " " + df.iloc[i, 1] + "###"
-    #print(df.iloc[i, 0])
     
 df.to_csv('therapy_2000_c.csv', index=False)
