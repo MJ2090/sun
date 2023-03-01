c9c667e12293fe459a93f3f971163faf831cda9f
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-03-01 11:12:12 -0800

    s

--- mysite/fine/fine.py
+++ mysite/fine/fine.py
@@ -1,2 +1,10 @@
-def run():
-    pass
+import pandas as pd
+df = pd.read_csv('therapy_3.csv')
+
+
+for i in range(3):
+    df.iloc[i, 0] = df.iloc[i, 0] + "\n\n###\n\n"
+    df.iloc[i, 1] = " " + df.iloc[i, 1] + "\n###\n"
+    print(df.iloc[i, 0])
+    
+df.to_csv('therapy_3_a.csv', index=False)
