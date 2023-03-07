6d4e8d537ad0f9dde437064fcb87c284c1d6dd17
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-03-06 20:47:08 -0800

    s

--- mysite/embedding/openai/df.py
+++ mysite/embedding/openai/df.py
@@ -160,7 +160,10 @@ def generate_embedding_csv():
     ################################################################################
 
     df = pd.read_csv('processed/embeddings.csv', index_col=0)
-    df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+    print(811, df)
+    df.to_csv('d32222.csv')
+    df = pd.read_csv('d32222.csv', index_col=0)
+    print(711, df)
 
     df.head()
 
