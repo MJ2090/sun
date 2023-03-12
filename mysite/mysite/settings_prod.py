e60744d444d441c718d3ffe2a437f438486fdfe0
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-03-12 11:28:45 -0700

    s

--- mysite/mysite/settings_prod.py
+++ mysite/mysite/settings_prod.py
@@ -11,12 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from mysite.settings_base import *
 
-print('im in prod............')
-DEBUG = False
-SECURE_HSTS_SECONDS = 300
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+print('In prod............')
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+DEBUG = False
