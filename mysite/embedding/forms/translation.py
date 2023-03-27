15f2fa9a5d5dfb2f027264333d9855592aa0536f
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-03-27 10:49:32 -0700

    s

--- mysite/embedding/forms/translation.py
+++ mysite/embedding/forms/translation.py
@@ -3,9 +3,9 @@ from django import forms
 import embedding.static_values as sc
 
 class TranslationForm(forms.Form):
-    text = forms.CharField(required=True, max_length=10000, widget=forms.Textarea(
-        attrs={'class': 'translation form-control', 'placeholder': "Limit 10,000 chars. "}))
-    translated_text = forms.CharField(required=True, max_length=4000, widget=forms.Textarea(
+    text = forms.CharField(required=True, max_length=4000, widget=forms.Textarea(
+        attrs={'class': 'translation form-control', 'placeholder': "Limit 4,000 chars. "}))
+    translated_text = forms.CharField(required=True, max_length=10000, widget=forms.Textarea(
         attrs={'disabled': 'true', 'class': 'translation form-control', 'placeholder': "... "}))
     target = forms.ChoiceField(required=True, choices=sc.TRANSLATION_TYPES, widget=forms.Select(
         attrs={'class': 'image-typle form-select', }))
