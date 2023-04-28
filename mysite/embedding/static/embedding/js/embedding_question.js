ea16dd5779fec8102f643afe2ffd8c06c2db9396
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-04-28 10:53:35 -0700

    s

--- mysite/embedding/static/embedding/js/embedding_question.js
+++ mysite/embedding/static/embedding/js/embedding_question.js
@@ -3,7 +3,6 @@ function embedding_question_fetch() {
   let character = $("select[name='character']");
   let csrf = $("input[name='csrfmiddlewaretoken']");
   let answer = $("textarea[name='answer']");
-  let enable_speech = $("input[name='enable_speech']");
   answer.val('');
   answer.hide();
   $("div[name='spinner").show();
@@ -11,7 +10,6 @@ function embedding_question_fetch() {
   const request_data = new FormData();
   request_data.append('question', question.val());
   request_data.append('character', character.val());
-  request_data.append('enable_speech', '');
   request_data.append('csrfmiddlewaretoken', csrf.val());
   fetch("/embedding_question_async/", {
     method: "POST",
@@ -19,38 +17,13 @@ function embedding_question_fetch() {
   }).then(response => response.json()).then((response) => {
     let data = response;
     let my_answer = data.answer
-    let audio_address = data.audio_address
     answer.val(my_answer);
     $("div[name='spinner").hide();
     answer.show();
-
-    if (enable_speech[0].checked && audio_address != '') {
-      let source = $("source[name='source']");
-      source.attr('src', '/static/embedding/media/' + audio_address + '.mp3');
-      let audio = $("audio[name='audio']");
-      audio[0].load();
-    }
   });
 }
 
 function embedding_question_init() {
-  // Audio play starts
-  $("input[name='enable_speech']").click(function () {
-    if (!this.checked) {
-      let audio = $("audio[name='audio']");
-      audio[0].pause();
-    }
-  });
-
-  $("input[name='show_controls']").click(function () {
-    let audio = $("audio[name='audio']").toggle(this.checked);
-  });
-
-  $("audio[name='audio']").on('canplaythrough', function () {
-    this.play();
-  });
-  // Audio play ends
-
   $('.send-button').click(function () {
     embedding_question_fetch();
     return false;
