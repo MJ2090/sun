b371b1e48062fcd319ba88785d53db358625d439
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-04-30 11:54:48 -0700

    s

--- mysite/embedding/openai/df.py
+++ mysite/embedding/openai/df.py
@@ -121,19 +121,14 @@ def generate_embedding_csv():
     # Note that you may run into rate limit issues depending on how many files you try to embed
     # Please check out our rate limit guide to learn more on how to handle this: https://platform.openai.com/docs/guides/rate-limits
 
-    df['embeddings'] = df.text.apply(apply)
+    df['embeddings'] = df.text.apply(
+        lambda x: openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
     df.to_csv('processed/embeddings.csv')
     df.head()
     df = pd.read_csv('processed/embeddings.csv', index_col=0)
     df.head()
 
     return df
-
-
-def apply(x):
-    if isinstance(x, float) and np.isnan(x):
-        return ''
-    return openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding']
 
 
 def get_df(my_texts=None):
