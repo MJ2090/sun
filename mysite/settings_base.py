ab42189f9de21b48d792e7b9c6855fa5e21653bb
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-04 20:10:09 -0700

    s

--- mysite/settings_base.py
+++ mysite/settings_base.py
@@ -37,7 +37,6 @@ ALLOWED_HOSTS = ["52.10.1.27",
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'embedding.apps.EmbeddingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
