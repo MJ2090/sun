e17b19e95c98667f0ea85db75597445d6e6175c3
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-07 23:04:41 -0700

    s

--- embedding/ocr.py
+++ embedding/ocr.py
@@ -3,8 +3,9 @@ import os
 
 
 def read_image(file_name):
-    reader = easyocr.Reader(['en'], gpu=False) # this needs to run only once to load the model into memory
-    result = reader.readtext(file_name)
-    for item in result:
-        print(item[1])
+    result = "s"
+    # reader = easyocr.Reader(['en'], gpu=False) # this needs to run only once to load the model into memory
+    # result = reader.readtext(file_name)
+    # for item in result:
+    #     print(item[1])
     return result
