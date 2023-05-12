cfae5586c32fa65c1f6b7ad59b3fbba4a43d32cf
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-12 09:48:45 -0700

    s

--- embedding/ocr.py
+++ embedding/ocr.py
@@ -95,7 +95,7 @@ def ocr_xunfei(file_name):
     context = ""
     for page in finalResult_json['pages']:
         for line in page['lines']:
-            tmp = ' '.join([word['content']for word in line['words'] if word['conf']>0.85])
+            tmp = ' '.join([word['content']for word in line['words'] if word['conf']>0.95])
             context += tmp
     print(context)
     return context
