c8f6d96109c13710a111b844412d3f1fa6982b05
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-12 08:58:09 -0700

    s

--- embedding/ocr.py
+++ embedding/ocr.py
@@ -3,12 +3,23 @@ import os
 import requests
 from PIL import Image
 import pytesseract
+from datetime import datetime
+from wsgiref.handlers import format_date_time
+from time import mktime
+import hashlib
+import base64
+import hmac
+from urllib.parse import urlencode
+import json
 
-def read_image(file_name, source='tesseract'):
+
+def read_image(file_name, source='xunfei'):
     if source=='osr_space':
         return ocr_space(file_name)
     if source=='tesseract':
         return ocr_tesseract(file_name)
+    if source=='xunfei':
+        return ocr_xunfei(file_name)
     return "Nothing, no source found"
 
 
@@ -31,3 +42,109 @@ def ocr_space(file_name):
                           data=payload,
                           )
     return r.content.decode()
+
+
+def ocr_xunfei(file_name):
+    APPId = "9153216a"  # 控制台获取
+    APISecret = "NzY2ZTQzMjg4ZTE1ODgwN2Q0OGI2YzUx"  # 控制台获取
+    APIKey = "8c236684482552b6f3dff60d6d8b4065"  # 控制台获取
+
+    with open(file_name, "rb") as f:
+        imageBytes = f.read()
+    url = 'https://api.xf-yun.com/v1/private/sf8e6aca1'
+
+    body = {
+        "header": {
+            "app_id": APPId,
+            "status": 3
+        },
+        "parameter": {
+            "sf8e6aca1": {
+                "category": "ch_en_public_cloud",
+                "result": {
+                    "encoding": "utf8",
+                    "compress": "raw",
+                    "format": "json"
+                }
+            }
+        },
+        "payload": {
+            "sf8e6aca1_data_1": {
+                "encoding": "jpg",
+                "image": str(base64.b64encode(imageBytes), 'UTF-8'),
+                "status": 3
+            }
+        }
+    }
+
+    request_url = assemble_ws_auth_url(url, "POST", APIKey, APISecret)
+
+    headers = {'content-type': "application/json", 'host': 'api.xf-yun.com', 'app_id': APPId}
+    print(request_url)
+    response = requests.post(request_url, data=json.dumps(body), headers=headers)
+    # print(response)
+    # print(response.content)
+
+    # print("resp=>" + response.content.decode())
+    tempResult = json.loads(response.content.decode())
+
+    finalResult = base64.b64decode(tempResult['payload']['result']['text']).decode()
+    finalResult = finalResult.replace(" ", "").replace("\n", "").replace("\t", "").strip()
+    # print("text字段Base64解码后=>" + finalResult)
+    finalResult_json = json.loads(finalResult)
+    context = ""
+    for page in finalResult_json['pages']:
+        for line in page['lines']:
+            tmp = ' '.join([word['content']for word in line['words']])
+            context += tmp
+    print(context)
+    return context
+
+"""Private functions ..."""
+
+class Url:
+    def __init__(self, host, path, schema):
+        self.host = host
+        self.path = path
+        self.schema = schema
+        pass
+
+
+def parse_url(requset_url):
+    stidx = requset_url.index("://")
+    host = requset_url[stidx + 3:]
+    schema = requset_url[:stidx + 3]
+    edidx = host.index("/")
+    if edidx <= 0:
+        print("edidx <= 0")
+    path = host[edidx:]
+    host = host[:edidx]
+    u = Url(host, path, schema)
+    return u
+
+
+# build websocket auth request url
+def assemble_ws_auth_url(requset_url, method="POST", api_key="", api_secret=""):
+    u = parse_url(requset_url)
+    host = u.host
+    path = u.path
+    now = datetime.now()
+    date = format_date_time(mktime(now.timetuple()))
+    print(date)
+    # date = "Thu, 12 Dec 2019 01:57:27 GMT"
+    signature_origin = "host: {}\ndate: {}\n{} {} HTTP/1.1".format(host, date, method, path)
+    print(signature_origin)
+    signature_sha = hmac.new(api_secret.encode('utf-8'), signature_origin.encode('utf-8'),
+                             digestmod=hashlib.sha256).digest()
+    signature_sha = base64.b64encode(signature_sha).decode(encoding='utf-8')
+    authorization_origin = "api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"" % (
+        api_key, "hmac-sha256", "host date request-line", signature_sha)
+    authorization = base64.b64encode(authorization_origin.encode('utf-8')).decode(encoding='utf-8')
+    print(authorization_origin)
+    values = {
+        "host": host,
+        "date": date,
+        "authorization": authorization
+    }
+
+    return requset_url + "?" + urlencode(values)
