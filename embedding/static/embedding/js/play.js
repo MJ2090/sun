c1c1a502df3f20fa8530f35a07bfc65b43b31a95
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-13 20:06:18 -0700

    s

--- embedding/static/embedding/js/play.js
+++ embedding/static/embedding/js/play.js
@@ -2,7 +2,12 @@ function play_fetch() {
     let original_iamge = document.querySelector("input[name='image_f']");
     let csrf = document.querySelector("input[name='csrfmiddlewaretoken']");
     let llm_model = document.querySelector("select[name='llm_model']");
+    let question = document.querySelector("textarea[name='response_question']")
+    let answer = document.querySelector("textarea[name='response_answer']")
 
+    question.value = '';
+    answer.value = '';
+    
     const request_data = new FormData();
     request_data.append('original_iamge', original_iamge.files[0]);
     request_data.append('csrfmiddlewaretoken', csrf.value);
@@ -15,8 +20,6 @@ function play_fetch() {
         body: request_data,
     }).then(response => response.json()).then((response) => {
         let data = response;
-        question = document.querySelector("textarea[name='response_question']")
-        answer = document.querySelector("textarea[name='response_answer']")
         question.value = data.question;
         answer.value = data.answer;
         spinner.style.display = 'none';
