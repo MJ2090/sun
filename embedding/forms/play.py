1dae3fd4813ce87b0455decab3c819313fc62709
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-21 18:32:43 -0700

    s

--- embedding/forms/play.py
+++ embedding/forms/play.py
@@ -4,6 +4,6 @@ import embedding.static_values as sc
 
 class PlayForm(forms.Form):
     image_f = forms.ImageField(required=False, widget=forms.FileInput(
-        attrs={'class': 'form-control'}))
+        attrs={'class': 'form-control hidden'}))
     llm_model = forms.ChoiceField(required=True, choices=sc.TRAINING_MODELS, widget=forms.Select(
         attrs={'class': 'character form-select', }))
