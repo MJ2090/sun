abef501a999ba98e6af4617e46ed08d7d282aa73
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-23 15:24:42 -0700

    s

--- embedding/forms/play.py
+++ embedding/forms/play.py
@@ -5,4 +5,4 @@ import embedding.static_values as sc
 class PlayForm(forms.Form):
     image_f = forms.ImageField(required=False, widget=forms.FileInput(
         attrs={'class': 'form-control hidden'}))
-    llm_model = forms.ChoiceField(required=True, choices=sc.QUIZ_MODELS, widget=forms.RadioSelect())
+    llm_model = forms.ChoiceField(required=True, choices=sc.QUIZ_MODELS, widget=forms.RadioSelect(attrs={'class': 'mt-1'}))
