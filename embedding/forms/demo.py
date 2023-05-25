b438c357f7b1222ff38144fd17a4b332e05919f3
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-24 18:11:29 -0700

    s

--- embedding/forms/demo.py
+++ embedding/forms/demo.py
@@ -9,7 +9,7 @@ class DemoForm(forms.Form):
     result_text = forms.CharField(required=True, max_length=10000, widget=forms.Textarea(
         attrs={'disabled': 'true', 'class': 'summary form-control', 'placeholder': "... "}))
     temperature = forms.FloatField(required=True, widget=forms.NumberInput(
-        attrs={'class': 'form-control', 'placeholder': "0.9"}))
+        attrs={'class': 'form-control', 'value': "0.9"}))
     character = forms.ChoiceField(required=True, choices=(), widget=forms.Select(
         attrs={'class': 'character form-select', }))
     question = forms.CharField(required=True, max_length=500, widget=forms.Textarea(
