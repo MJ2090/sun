2f1189635f572fe378c2f37e37c1cfacc738d4b7
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-29 14:05:29 -0700

    s

--- therapy/urls.py
+++ therapy/urls.py
@@ -4,7 +4,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
     path('therapy/', views.therapy, name='therapy'),
 ]
 
--- therapy/views.py
+++ therapy/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 
-
 def therapy(request):
     ret = {}
     return render(request, 'therapy/home.html', ret)
