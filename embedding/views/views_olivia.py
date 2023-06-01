80748d89330ff6d866f92241b2475d11d483d74c
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-01 11:07:20 -0700

    s

--- embedding/views/views_olivia.py
+++ embedding/views/views_olivia.py
@@ -9,7 +9,7 @@ import embedding.static_values as sc
 import json
 
 
-def chat_async_olivia(request):
+def gchat_async_olivia(request):
     model = 'gpt-4'
     new_message = request.POST['message']
     character = 'TT_3'
