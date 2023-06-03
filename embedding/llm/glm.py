a25c5667e8d97a9de9c58c45460c0fb154ad57c4
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-03 09:15:26 -0700

    s

--- embedding/llm/glm.py
+++ embedding/llm/glm.py
@@ -4,8 +4,8 @@ import json
 
 def create(request, messages, prompt, temperature):
     url = 'https://llm.asuperdomain.com/glm_async/'
-    print(f"create: {messages}, temperature: {temperature}")
+    print(f"create: messages:\n {messages}, temperature:\n {temperature}, prompt:\n {prompt}")
     post_response = requests.Session().post(url, data={
         'messages': json.dumps(messages), 'prompt': prompt, 'temperature': temperature}, headers=dict(Referer=url))
-    print("post_response: ", post_response.json())
+    print("post_response: \n", post_response.json())
     return post_response.json()
