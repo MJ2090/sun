733214d04542e417c9cf4a23e034f8810ab5d313
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-05 11:13:41 -0700

    s

--- embedding/static/embedding/js/wuxi.js
+++ embedding/static/embedding/js/wuxi.js
@@ -18,7 +18,7 @@ function fetch_documents() {
 
     for (let i = 0; i < data.length; i++) {
       let a = document.createElement('a');
-      a.href = data[i]
+      a.href = '/static/embedding/' + data[i]
       a.innerHTML = data[i].split("/").slice(-1)[0].substring(16);
       let new_li = li.cloneNode();
       new_li.classList.remove("hidden");
