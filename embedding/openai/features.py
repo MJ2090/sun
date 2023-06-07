b88f307dc168ac331a2675724fb0b52ef010c946
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-07 01:41:35 -0700

    s

--- embedding/openai/features.py
+++ embedding/openai/features.py
@@ -124,12 +124,12 @@ def feature_grammar(original_text, model):
 
 def feature_summary(original_text, model, max_words=0, max_tokens=1500):
     messages = [
-        {"role": "system", "content": "Generate a summarization for the Input Text. Step #1. determine which language is using in the Input Text. Step #2. write a summary based on the Input Text in the same language."},
+        {"role": "system", "content": "Summarize the Input Text. Step #1. determine which language is used in the Input Text. Step #2. write a summary based on the Input Text in the same language."},
         {"role": "user", "content": "The summarization MUST use the same language as the input text."},
         {"role": "user", "content": f"Input Text:\n\n\n {original_text}"},
     ]
     if max_words > 0:
-        messages[0]['content'] += f" The summarization should be within {max_words} words."
+        messages[0]['content'] += f" The summarization MUST be within {max_words} words."
     try:
         response = openai.ChatCompletion.create(
             model=model,
