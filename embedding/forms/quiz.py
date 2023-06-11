3e080f0fe70186cfd819e2b1c9b1082f4008a0b8
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-11 15:42:33 -0700

    s

--- embedding/forms/quiz.py
+++ embedding/forms/quiz.py
@@ -6,4 +6,4 @@ class QuizForm(forms.Form):
     image_f = forms.ImageField(required=False, widget=forms.FileInput(
         attrs={'class': 'form-control hidden'}))
     llm_model = forms.ChoiceField(required=False, choices=sc.QUIZ_MODELS, widget=forms.RadioSelect(attrs={'class': 'mt-1'}))
-    question_type = forms.ChoiceField(required=False, choices=sc.Q_TYPES, widget=forms.RadioSelect(attrs={'class': 'mt-1 mb-3 question-type'}))
+    question_type = forms.ChoiceField(required=False, choices=sc.Q_TYPES, widget=forms.RadioSelect(attrs={'class': 'mt-1 question-type'}))
