b3a7a930dca3dac1f51accaf051fde4b4f083bed
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-17 01:21:16 -0700

    s

--- embedding/views/views_tele.py
+++ embedding/views/views_tele.py
@@ -1,19 +1,37 @@
 from django.http import HttpResponse
-from embedding.forms.grammar import GrammarForm
-from embedding.openai.features import feature_grammar
-from django.shortcuts import render
-from embedding.utils import get_basic_data
-import embedding.static_values as sc
 import json
-import stripe
-from django.http import HttpResponseRedirect
-from django.conf import settings as conf_settings
 from django.views.decorators.csrf import csrf_exempt
+from telegram import Bot
+from telegram.error import TelegramError
+from telegram import Update
+from telegram.ext import Dispatcher, CommandHandler
+from telegram.ext import CallbackContext
 
 
+def send_telegram_message(bot_token, chat_id, message):
+    try:
+        bot = Bot(token=bot_token)
+        bot.send_message(chat_id=chat_id, text=message)
+        return True
+    except TelegramError as e:
+        # Handle any errors that occur during sending the message
+        print(f"Telegram Error: {str(e)}")
+        return False
+    
 @csrf_exempt
 def tele(request):
-    print("33333333333333333333300000000000000000000", request.POST, request.GET, request['result'])
+    print("33333333333333333333300000000000000000000", request.body, request.GET, request['result'])
+    # telegram_request = Request(
+    #     request=request.body,
+    #     headers=request.headers,
+    #     input_stream=request,
+    # )
+
+    bot_token = "6186366547:AAHgkEeWAt_IkWJfxRvQGdRwB2P-ZIOprGY"
+    message = "Hello, Telegram!"
+    chat_id = "YOUR_CHAT_ID"
+
+    # send_telegram_message(bot_token, chat_id, message)
     return HttpResponse(json.dumps({'question': 'okk'}))
 
 # curl -F "url=https://www.asuperdomain.com/tele" -F "certificate=@a.pem" https://api.telegram.org/bot6186366547:AAHgkEeWAt_IkWJfxRvQGdRwB2P-ZIOprGY/getWebhookInfo
