309609013fa939a66797822b419783307c423055
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-17 01:42:43 -0700

    s

--- embedding/views/views_tele.py
+++ embedding/views/views_tele.py
@@ -3,12 +3,14 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from telegram import Bot
 from telegram.error import TelegramError
+import asyncio
 
 
-async def send_telegram_message(bot_token, chat_id, message):
+def send_telegram_message(chat_id=6208450080, message="343434"):
     try:
+        bot_token = "6186366547:AAHgkEeWAt_IkWJfxRvQGdRwB2P-ZIOprGY"
         bot = Bot(token=bot_token)
-        await bot.send_message(chat_id=chat_id, text=message)
+        bot.send_message(chat_id=chat_id, text=message)
         return True
     except TelegramError as e:
         # Handle any errors that occur during sending the message
@@ -17,6 +19,9 @@ async def send_telegram_message(bot_token, chat_id, message):
     
 @csrf_exempt
 def tele(request):
+    loop = asyncio.get_running_loop()
+    task = loop.create_task(send_telegram_message())
+    loop.run_until_complete(task)
     print("33333333333333333333300000000000000000000", request.body)
     # telegram_request = Request(
     #     request=request.body,
@@ -24,11 +29,10 @@ def tele(request):
     #     input_stream=request,
     # )
 
-    bot_token = "6186366547:AAHgkEeWAt_IkWJfxRvQGdRwB2P-ZIOprGY"
     message = "Hello, Telegram!"
     chat_id = 6208450080
 
-    send_telegram_message(bot_token, chat_id, message)
+    # send_telegram_message(bot_token, chat_id, message)
     return HttpResponse(json.dumps({'question': 'okk'}))
 
 # curl -F "url=https://www.asuperdomain.com/tele" -F "certificate=@a.pem" https://api.telegram.org/bot6186366547:AAHgkEeWAt_IkWJfxRvQGdRwB2P-ZIOprGY/getWebhookInfo
