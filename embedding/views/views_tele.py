177a76691d3d39fe9d053048723a8f642905118a
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-17 00:34:02 -0700

    s

--- embedding/views/views_tele.py
+++ embedding/views/views_tele.py
@@ -13,7 +13,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def tele(request):
-    print("33333333333333333333300000000000000000000", request.POST)
+    print("33333333333333333333300000000000000000000", request.POST, request.GET)
     return HttpResponse(json.dumps({'question': 'okk'}))
 
 # curl -F "url=https://www.asuperdomain.com/tele" -F "certificate=@a.pem" https://api.telegram.org/bot6186366547:AAHgkEeWAt_IkWJfxRvQGdRwB2P-ZIOprGY/getWebhookInfo
