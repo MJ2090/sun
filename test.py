df42393e04dc1b29f7db3ac87818aaa1b6ba81b6
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-18 12:22:39 -0700

    s

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from threading import Thread
 from time import sleep
+import pandas
+import ast
+import numpy as np
+
 
 def threaded_function(arg):
     for i in range(arg):
@@ -7,8 +11,27 @@ def threaded_function(arg):
         sleep(1)
 
 
+def test2(question, embedding_model, llm_model='gpt-3.5-turbo-16k'):
+    # def safe_literal_eval(value):
+    #     try:
+    #         return ast.literal_eval(value)
+    #     except (ValueError, SyntaxError):
+    #         return np.nan
+    # relative_path = ''
+    # file_path = relative_path + embedding_model.uuid + '.csv'
+    # if not os.path.exists(file_path):
+    #     return embedding_model.reject_message
+    # my_df = pd.read_csv(file_path, index_col=0, nrows=5280).dropna()
+    # print("333333333 1")
+    # # my_df['embeddings'] = my_df['embeddings'].apply(eval).apply(np.array)
+    # my_df['embeddings'] = my_df['embeddings'].apply(safe_literal_eval).apply(np.array)
+    # print("333333333 111")
+    # if llm_model == 'glm':
+    #     ans = robot.answer_question_glm(my_df, question=question)
+    # else:
+    #     ans = robot.answer_question_openai(
+    #         my_df, question=question, debug=True, reject_message=embedding_model.reject_message)
+    # return ans
+
 if __name__ == "__main__":
-    thread = Thread(target = threaded_function, args = (10, ))
-    thread.start()
-    # thread.join()
-    print("thread finished...exiting")
+    test2()
