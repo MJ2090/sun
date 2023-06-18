43766fee439df04cd1ef855c39eb3c5f187391cd
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-18 10:14:42 -0700

    s

--- embedding/openai/features.py
+++ embedding/openai/features.py
@@ -49,7 +49,7 @@ def feature_question(question, embedding_model, llm_model='gpt-3.5-turbo-16k'):
     file_path = relative_path + embedding_model.uuid + '.csv'
     if not os.path.exists(file_path):
         return embedding_model.reject_message
-    my_df = pd.read_csv(file_path, index_col=0, nrows=5800)
+    my_df = pd.read_csv(file_path, index_col=0, nrows=5400)
     print("333333333 1")
     # my_df['embeddings'] = my_df['embeddings'].apply(eval).apply(np.array)
     my_df['embeddings'] = my_df['embeddings'].apply(safe_literal_eval).apply(np.array)
