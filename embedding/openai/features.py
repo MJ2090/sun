9434e9a2f50a5e8277347bc3807a3880f48eb7f6
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-18 13:38:01 -0700

    s

--- embedding/openai/features.py
+++ embedding/openai/features.py
@@ -46,14 +46,15 @@ def feature_question(question, embedding_model, llm_model='gpt-3.5-turbo-16k'):
         except (ValueError, SyntaxError):
             print("value=", value, type(value), index)
             return np.nan
-        
+
     file_path = relative_path + embedding_model.uuid + '.csv'
     if not os.path.exists(file_path):
         return embedding_model.reject_message
     my_df = pd.read_csv(file_path, index_col=0, nrows=5280).dropna()
     print("333333333 1")
     # my_df['embeddings'] = my_df['embeddings'].apply(eval).apply(np.array)
-    my_df['embeddings'] = my_df['embeddings'].apply(lambda row, index: safe_literal_eval(row, index)).apply(np.array)
+    my_df['embeddings'] = my_df['embeddings'].apply(lambda row, index: safe_literal_eval(
+        row, index), args=(df['embeddings'].index,)).apply(np.array)
     print("333333333 111")
     if llm_model == 'glm':
         ans = robot.answer_question_glm(my_df, question=question)
