8e17bfe12710a80a26922467c9be4d6ae7310220
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-23 15:02:59 -0700

    s

--- embedding/views/views_pay.py
+++ embedding/views/views_pay.py
@@ -3,8 +3,6 @@ from embedding.forms.grammar import GrammarForm
 from embedding.openai.features import feature_grammar
 from django.shortcuts import render
 from embedding.utils import get_basic_data
-import embedding.static_values as sc
-import json
 import stripe
 from django.http import HttpResponseRedirect
 from django.conf import settings as conf_settings
