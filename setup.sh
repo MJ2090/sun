fb7b57d20d9c175bcd9807547ae9432709f7792a
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-06-23 15:00:57 -0700

    s

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-pip3 install django-ratelimit
+sudo apt update
+sudo apt install python3-pip -y
+sudo apt install mlocate -y
+sudo apt install nginx -y
+pip install django
+pip install django-ratelimit
+pip install openai
+pip install gunicorn
+pip install stripe
+pip install numpy
+pip install pandas
