ed3bdcb3a8d9dfb391b0633b56121390576460c8
mj <125602296+MJ2090@users.noreply.github.com>
2023-06-23 21:28:52 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,22 @@ pip install gunicorn
 pip install stripe
 pip install numpy
 pip install pandas
+pip install tiktoken
+pip install mysql-connector-python
+echo "BASE ended =============================="
+
+echo "DJANGO started =============================="
+echo "export PYTHONPATH=~/moon:$PYTHONPATH" >> ~/.bashrc
+echo "export SECRET_KEY='$(openssl rand -hex 40)'" > ~/.DJANGO_SECRET_KEY
+source ~/.DJANGO_SECRET_KEY
+echo "DJANGO ended =============================="
+
+echo "BASH started =============================="
+echo 'export LD_LIBRARY_PATH=:/usr/lib/x86_64-linux-gnu/' >> ~/.bashrc
+echo 'alias gl="git pull"' >> ~/.bashrc
+echo 'alias dr="python3 manage.py runserver 0.0.0.0:8000"' >> ~/.bashrc
+echo 'alias p3="python3"' >> ~/.bashrc
+echo 'alias dc="python3 manage.py collectstatic"' >> ~/.bashrc
+echo 'alias gk="pkill gunicorn"' >> ~/.bashrc
+echo 'alias gs="gunicorn -c config/gunicorn/prod.py"' >> ~/.bashrc
+echo "BASH ended =============================="
